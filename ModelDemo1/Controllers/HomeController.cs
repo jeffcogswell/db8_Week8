f9ad5bc28db0139137827e763f5f2d978bfb3a8d
Jeff Cogswell <jeffcogs@gmail.com>
2022-09-21 10:53:54 -0400

    Wednesday Morning

---
+++ ModelDemo1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelDemo1.Models;
+using System.Diagnostics;
+
+namespace ModelDemo1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult Sample()
+		{
+			Person fred = new Person() { FirstName = "Fred", 
+				LastName = "Franklin", Email = "fred@abc.com", Born = 1970 };
+			return View(fred);
+		}
+
+		public IActionResult ShowRegister()
+		{
+			return View();
+		}
+
+		public IActionResult Register(Person per)
+		{
+			return View(per);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
