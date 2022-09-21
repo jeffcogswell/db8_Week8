d97f037426fea356579abb5a8209ea9bf7e78be3
Jeff Cogswell <jeffcogs@gmail.com>
2022-09-21 16:44:41 -0400

    Wednesday afternoon

--- ModelDemo1/Controllers/HomeController.cs
+++ ModelDemo1/Controllers/HomeController.cs
@@ -30,6 +30,31 @@ namespace ModelDemo1.Controllers
 			return View(fred);
 		}
 
+		public IActionResult SampleList()
+		{
+			List<Person> people = new List<Person>();
+			Person per = new Person() { FirstName = "Fred", 
+				LastName = "Franklin", Email = "fred@abc.com", Born = 1970 };
+			people.Add(per);
+			per = new Person() { FirstName = "Sally",
+				LastName = "Smith", Email = "sally@abc.com", Born = 1990};
+			people.Add(per);
+			per = new Person() { FirstName = "Julia",
+				LastName = "Jones", Email = "julia@abc.com", Born = 1980 };
+			people.Add(per);
+
+			//foreach (Person next in people)
+			//{
+			//	Console.WriteLine($"{next.FirstName} {next.LastName}");
+			//}
+
+			//ViewBag.Department = "IT";
+			ViewData["Department"] = "IT";
+			ViewData["Year"] = 2022;
+
+			return View(people);
+		}
+
 		public IActionResult ShowRegister()
 		{
 			return View();
@@ -37,7 +62,17 @@ namespace ModelDemo1.Controllers
 
 		public IActionResult Register(Person per)
 		{
-			return View(per);
+			if (per.Born > 2004)
+			{
+				// Let's use a different view for people under 18
+				// We'll call the view "Under18.cshtml".
+				return View("Under18");
+				//return Redirect("/home/index");
+			}
+			else
+			{
+				return View(per);
+			}
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
